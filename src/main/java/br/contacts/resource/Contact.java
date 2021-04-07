5cdcd9c460dd0a782d5650626193eac11130d0a7
edesoaresm <edemarinho@outlook.com>
2021-04-07 17:45:32 -0300

    Subi o projeto

---
+++ src/main/java/br/contacts/resource/Contact.java
@@ -0,0 +1,42 @@
+package br.contacts.resource;
+
+public class Contact {
+
+	public Contact(Integer id, String nome, Integer telefone, String email) {
+		this.id = id;
+		this.nome = nome;
+		this.telefone = telefone;
+		this.email = email;
+	}
+	public Contact() {
+	}
+	private Integer id;
+	private String nome;
+	private Integer telefone;
+	private String email;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(Integer telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
